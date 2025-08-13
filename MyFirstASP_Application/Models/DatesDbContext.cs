3e93782404b17c38d26591bb78684d7f18458fc0
Kyrylo <popovkirill070320@gmail.com>
2025-08-13 23:52:03 +0300

    Реалізована логіка додавання дат

---
+++ MyFirstASP_Application/Models/DatesDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyFirstASP_Application.Models
+{
+    public class DatesDbContext : DbContext
+    {
+        public DbSet<Date> dates { get; set; }
+
+        public DatesDbContext(DbContextOptions<DatesDbContext> options) : base(options) { }
+    }
+}
